70a99c45e25dfd954056fb63888f9e119d47de84
v1chi <maria.quiroga@alumnos.ucn.cl>
2025-10-20 23:40:13 -0300

    cambios en Home
    se muestran botones para cada modulo, se presiona y se redirecciona a la pantalla de este

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import './Home.css';
 
 const Home = () => {
-    return(
-        <div>
-            <h1>Bienvenido al ERP</h1>
-            <p>Pagina en proceso</p>
-        </div>
-    )
-}
+  return (
+    <div className="home-container">
+      <h1>Bienvenido al Mini-ERP</h1>
+      <p>Selecciona el módulo al que deseas ingresar:</p>
+
+      <div className="module-grid">
+        {/* 1. Botón de Inventario (Aún sin navegación) */}
+        <button className="module-button">
+          Inventario
+        </button>
+
+        {/* 2. Botones de otros módulos (Deshabilitados) */}
+        <button className="module-button disabled" disabled>
+          Ventas
+        </button>
+
+        <button className="module-button disabled" disabled>
+          Compras
+        </button>
+
+        <button className="module-button disabled" disabled>
+          RRHH
+        </button>
+        
+        <button className="module-button disabled" disabled>
+          Logística/Despacho
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
