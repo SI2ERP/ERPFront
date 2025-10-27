7e8cb12620a4d6f2f7124303190d7082f86c192e
javierm-0 <javier.moraga@alumnos.ucn.cl>
2025-10-26 21:39:02 -0300

    redireccion por rol admin

--- src/Modulo de Recursos Humanos/Login_rrhh.tsx
+++ src/Modulo de Recursos Humanos/Login_rrhh.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+type Empleado = {
+  id: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  rol: string;
+  roles: string[];
+};
 
 export const Login_rrhh = () =>{
     const [correo,setCorreo] = useState("");
     const [pass,setPass] = useState("");
+    const [loginError, setLoginError] = useState(false);
+    const navigate = useNavigate();
+    const [empleado, setEmpleado] = useState<Empleado | null>(null);
+    
 
     const handleLogin = async (e: React.FormEvent) =>{
         e.preventDefault();
@@ -15,16 +29,31 @@ export const Login_rrhh = () =>{
                 password: pass
             }
             const res = await axios.post(url,bodyJson);
-            if(res.status == 201){
-                console.log("puede entrar");
-
+            if(res.status === 201){
+                const empleadoData: Empleado = res.data.empleado;
+                setEmpleado(empleadoData);
+                setLoginError(false);
+                if (empleadoData.rol === "Administrador") {
+                    console.log("admin");
+                    navigate("/rrhh/admin/");
+                } else {
+                    console.log("empleado");
+                    navigate("/rrhh/empleado/");
+                }
             }
             else{
                 console.warn("credenciales invalidas o error de conexion");
             }
 
-        } catch (error) {
-            console.error("ocurrio un error: ",error);
+        } catch (error : any) {
+            if(error.response.status === 401){
+                console.warn("credenciales invalidas");
+                setLoginError(true);
+            }
+            else{
+                console.error("ocurrio un error: ",error);
+            }
+            
         }
         // redirige a /rrhh/admin o a /rrhh/empleado dependiendo si contiene el rol admin o no
     };
@@ -58,6 +87,12 @@ export const Login_rrhh = () =>{
                     onChange={(e) => setPass(e.target.value)}
                     />
                 </div>
+
+                {loginError && (
+                    <p className="text-red-700 bg-red-200 p-2 rounded mt-2 mb-6">
+                        Credenciales incorrectas. Intente de nuevo.
+                    </p>
+                )}
 
                 <div className="flex justify-center mt-10">
                     <button
